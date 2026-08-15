29b8363e94ce3278e9b1dc6d1f796c54b589e112
loveboba <tshreya616@gmail.com>
2026-08-15 12:18:18 -0700

    basic database connection

--- backend/getting_response.py
+++ backend/getting_response.py
@@ -1,10 +1,31 @@
 from google import genai 
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
+import os 
+
+# import sqlalchemy
+import psycopg2 
+
+
+from sqlalchemy import create_engine, text, connectors
 
 # get the .env variable 
 load_dotenv()
 
 # connect to database 
+DATABASE_CONNECT = os.getenv("DATABASE_CONNECTION")
+
+the_engine = create_engine(DATABASE_CONNECT, echo=True)
+
+with the_engine.connect() as dbconnect: 
+
+    dbconnect.execute(text("INSERT INTO newsletter_table (the_date, the_newsletter) VALUES ('08/08/2004', 'bye')"))
+    dbconnect.commit()
+
+    newsletter_table = dbconnect.execute(text("SELECT * FROM newsletter_table"))
+
+    for row in newsletter_table:
+        print(row)
+    # print(newsletter_table.fetchall())
 
 
 # the_ai = genai.Client()
